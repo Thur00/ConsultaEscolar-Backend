bc07b145d1baa9b734e2fbeeeae7080dc74637cc
Thur00 <arthursvinhedo@gmail.com>
2024-06-10 16:08:34 -0300

    Padrão MVC

--- controllers/professoresController.js
+++ controllers/professoresController.js
@@ -1,5 +1,5 @@
 //clienteController.js
-const Professores = require('../models/professorerModel');
+const Professores = require('../models/professoresModel');
 // Controlador para obter todos os clientes
 exports.getProfessores = (req, res) => {
     Professores.getProfessores((err, professores) => {
